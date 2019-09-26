0ac94b659d1192cda2dc6296c9e6409c5a96bed8
Denis Nekrylov <neodenit@gmail.com>
2019-09-26 21:52:06 +0300

    Fix UI issues

--- ReadingListPlus.Web.Core/wwwroot/js/App/Common.js
+++ ReadingListPlus.Web.Core/wwwroot/js/App/Common.js
@@ -1,3 +1,13 @@
 ﻿$(function () {
-    $('.focused').focus();
+    function isMobile() {
+        var minDesktopSize = 768;
+        return screen.width < minDesktopSize;
+    }
+
+    var rows = isMobile() ? 3 : 10;
+    $('textarea').prop('rows', rows);
+
+    $('form').submit(function () {
+        $(this).find('input[type="submit"]').prop('disabled', true);
+    });
 });
