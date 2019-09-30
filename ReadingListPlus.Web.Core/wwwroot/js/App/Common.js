1d6237bda460020a255c0f3012841960e5ef2397
Denis Nekrylov <neodenit@gmail.com>
2019-09-30 22:19:30 +0300

    Fix input fields cursor position

--- ReadingListPlus.Web.Core/wwwroot/js/App/Common.js
+++ ReadingListPlus.Web.Core/wwwroot/js/App/Common.js
@@ -4,10 +4,37 @@
         return screen.width < minDesktopSize;
     }
 
-    var rows = isMobile() ? 3 : 10;
-    $('textarea').prop('rows', rows);
+    function updateRowNumber() {
+        var rows = isMobile() ? 3 : 10;
+        $('textarea').prop('rows', rows);
+    }
 
-    $('form').submit(function () {
-        $(this).find('input[type="submit"]').prop('disabled', true);
-    });
+    function fixCursorPosition(element) {
+        var len = element.value.length;
+        element.setSelectionRange(len, len);
+    }
+
+    function fixPageCursorPosition() {
+        var input = $('input[autofocus]')[0];
+
+        if (input) {
+            fixCursorPosition(input);
+        } else {
+            var textarea = $('textarea[autofocus]')[0];
+
+            if (textarea && textarea.clientHeight >= textarea.scrollHeight) {
+                fixCursorPosition(textarea);
+            }
+        }
+    }
+
+    function preventDoubleSubmit() {
+        $('form').submit(function () {
+            $(this).find('input[type="submit"]').prop('disabled', true);
+        });
+    }
+
+    updateRowNumber();
+    fixPageCursorPosition();
+    preventDoubleSubmit();
 });
