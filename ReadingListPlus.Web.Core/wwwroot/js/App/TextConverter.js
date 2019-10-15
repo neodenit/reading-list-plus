22a9e60fe550dce37008487ee787a6e64007e7da
Denis Nekrylov <neodenit@gmail.com>
2019-10-15 20:14:38 +0300

    Fix client side new line handling

--- ReadingListPlus.Web.Core/wwwroot/js/App/TextConverter.js
+++ ReadingListPlus.Web.Core/wwwroot/js/App/TextConverter.js
@@ -1,9 +1,11 @@
-﻿var extractRegex = /<a href=".+?" class="(\w+)" data-id-param="([0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12})">([\s\S]+?)<\/a>/g;
+﻿var newLineRegex = /\r|\n/g;
+var extractRegex = /<a href=".+?" class="(\w+)" data-id-param="([0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12})">([\s\S]+?)<\/a>/g;
 var spanRegex = /<span class="(\w+)">([\s\S]+?)<\/span>/g;
 var brRegex = /<br\s*\/?>/g;
 
 function HtmlToText(html) {
-    var textWithNoExtracts = html.replace(extractRegex, '{{$1::$2::$3}}');
+    var textWithNoNewLines = html.replace(newLineRegex, '');
+    var textWithNoExtracts = textWithNoNewLines.replace(extractRegex, '{{$1::$2::$3}}');
     var textWithNoSpans = textWithNoExtracts.replace(spanRegex, '{{$1::$2}}');
     var textWithNoBRs = textWithNoSpans.replace(brRegex, '\r\n');
 
