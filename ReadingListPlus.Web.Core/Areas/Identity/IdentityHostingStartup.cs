0bf6e9594f7a8eb4f312297ff021503de2951fa8
Denis Nekrylov <neodenit@gmail.com>
2019-10-25 20:36:25 +0300

    Scaffold Login and Register pages

---
+++ ReadingListPlus.Web.Core/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ReadingListPlus.DataAccess;
+
+[assembly: HostingStartup(typeof(ReadingListPlus.Web.Core.Areas.Identity.IdentityHostingStartup))]
+namespace ReadingListPlus.Web.Core.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
