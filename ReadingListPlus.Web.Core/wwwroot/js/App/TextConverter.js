779a8dbabec001de00ec1f86c15f2d99faed6501
Denis Nekrylov <neodenit@gmail.com>
2019-11-08 21:33:26 +0200

    Fix HTML decoding

--- ReadingListPlus.Web.Core/wwwroot/js/App/TextConverter.js
+++ ReadingListPlus.Web.Core/wwwroot/js/App/TextConverter.js
@@ -1,15 +1,21 @@
 ﻿'use strict';
 
-var newLineRegex = /\r|\n/g;
-var extractRegex = /<a href=".+?" class="(\w+)" data-id-param="([0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12})">([\s\S]+?)<\/a>/g;
-var spanRegex = /<span class="(\w+)">([\s\S]+?)<\/span>/g;
-var brRegex = /<br\s*\/?>/g;
-
 function HtmlToText(html) {
+    var newLineRegex = /\r|\n/g;
+    var extractRegex = /<a href=".+?" class="(\w+)" data-id-param="([0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12})">([\s\S]+?)<\/a>/g;
+    var spanRegex = /<span class="(\w+)">([\s\S]+?)<\/span>/g;
+    var brRegex = /<br\s*\/?>/g;
+
+    function htmlDecode(value) {
+        var doc = new DOMParser().parseFromString(value, "text/html");
+        return doc.documentElement.textContent;
+    }
+
     var textWithNoNewLines = html.replace(newLineRegex, '');
     var textWithNoExtracts = textWithNoNewLines.replace(extractRegex, '{{$1::$2::$3}}');
     var textWithNoSpans = textWithNoExtracts.replace(spanRegex, '{{$1::$2}}');
     var textWithNoBRs = textWithNoSpans.replace(brRegex, '\r\n');
 
-    return textWithNoBRs;
+    var result = htmlDecode(textWithNoBRs);
+    return result;
 }
